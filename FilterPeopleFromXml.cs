ed1e777dbd167de2fe755a89f35dd22277e1ba1d
EmirhanTemel <emirhan.temel@ozu.edu.tr>
2025-09-02 18:25:54 +0300

    Expected I/O Formats
    Filled expected input/output formats for each file

---
+++ FilterPeopleFromXml.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+using System.Xml.Linq;
+using System.Text.Json;
+
+public class CaseStudy {
+    public static string FilterPeopleFromXml(string xmlData)
+    {
+
+    }
+
+}
---
+++ LongestVowelSubsequenceAsJson.cs
@@ -0,0 +1,11 @@
+using System.Linq;
+using System.Text.Json;
+using System.Collections.Generic;
+
+public class CaseStudy {
+    public static string LongestVowelSubSubsequenceAsJson(List<string> words)
+    {
+
+    }
+
+}
